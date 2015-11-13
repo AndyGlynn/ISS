1b18c14067a4f87ed0b60d0b02db7fcc3a3d377e
Aaron I. Clay <aaron.clay79@gmail.com>
2015-11-13 12:07:13 -0500

    Create PreviousCustomerFORM.vb

---
+++ PreviousCustomerFORM.vb
@@ -0,0 +1,6 @@
+Public Class PreviousCustomer
+
+    Private Sub PreviousCustomer_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = Main
+    End Sub
+End Class
