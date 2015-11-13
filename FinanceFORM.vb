25d54fd5b0aa4edc9eab0aee5f7a23adb9a033c2
Aaron I. Clay <aaron.clay79@gmail.com>
2015-11-13 11:41:10 -0500

    Create FinanceFORM.vb

---
+++ FinanceFORM.vb
@@ -0,0 +1,7 @@
+Public Class Finance
+
+    Private Sub Finance_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = Main
+
+    End Sub
+End Class
