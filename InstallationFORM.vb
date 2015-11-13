31bbd64dc9ab6ee534da3247a198b14d9699c707
Aaron I. Clay <aaron.clay79@gmail.com>
2015-11-13 11:55:34 -0500

    Create InstallationFORM.vb

---
+++ InstallationFORM.vb
@@ -0,0 +1,6 @@
+Public Class Installation
+
+    Private Sub Installation_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = Main
+    End Sub
+End Class
