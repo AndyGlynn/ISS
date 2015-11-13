1bca90f1366bfc388e98878f6d5779627e5c3cc0
Aaron I. Clay <aaron.clay79@gmail.com>
2015-11-13 11:25:38 -0500

    Create ColdCallingFORM.vb

---
+++ ColdCallingFORM.vb
@@ -0,0 +1,6 @@
+Public Class ColdCalling
+
+    Private Sub ColdCalling_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = Main
+    End Sub
+End Class
