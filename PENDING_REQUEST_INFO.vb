f26f36e1882706b814a690782babe654c2ba8ea3
Aaron I. Clay <aaron.clay79@gmail.com>
2015-11-13 12:06:28 -0500

    Create PENDING_REQUEST_INFO.vb

---
+++ PENDING_REQUEST_INFO.vb
@@ -0,0 +1,14 @@
+Module PENDING_REQUEST_INFO
+    Public Who As String = ""
+    Public LineHoldingOn As String = ""
+    Public XFER_NOTES As String = ""
+    Public LeadNumber As String = ""
+    Public Auto_RESPONSE As String = ""
+
+    Public GetStartupForm As Boolean = False
+
+    Public IncommingChatMsg As Boolean = False
+    Public Sender As String = ""
+    Public MSG As String = ""
+
+End Module
